b818e6bde11fbc6a7ef068f38ef0803cd5bfeefe
arnavbansal25 <arnavbansal.8232@gmail.com>
2023-08-15 23:03:33 +0530

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      hoverGrayBG: "#e5e5e5",
+      selectedGrayBG: "#f2f2f2",
+      selectedBlackBG: "#0e0e0f",
+      white: "#fff",
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
